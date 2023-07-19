67bd9c1593891f68eda55824a44958a1c044b336
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-07-19 18:03:17 +0000

    implements order repository methods

---
+++ internal/infra/database/order-Repository.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/nathanSeixeiro/go-intensivo/internal/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	// if we wanna ignore one value using _
+	_, err := r.Db.Exec("INSERT INTO orders (id, price, tax, final_price) VALUES (?,?,?,?)",
+		order.ID, order.Price, order.Tax, order.Finalp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *OrderRepository) GetTotalTransaction() (int, error) {
+	var total int
+	err := r.Db.QueryRow("SELECT COUNT (*) FROM orders").Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+	return total, nil
+}
