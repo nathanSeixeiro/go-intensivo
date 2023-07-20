4d1d904e9020fb8c646eb17cb751d3a94409aa81
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-07-20 05:11:23 +0000

    add rabbitmq

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nathanSeixeiro/go-intensivo/internal/infra/database"
 	usecases "github.com/nathanSeixeiro/go-intensivo/internal/useCases"
+	"github.com/nathanSeixeiro/go-intensivo/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -14,16 +17,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close() // waited run all of them and after close connection
 	orderRepository := database.NewOrderRepository(db)
-	usecase := usecases.NewCalculateFinalPrice(orderRepository)
-	input := usecases.OrderInput{
-		ID: "1",
-		Price: 10.0,
-		Tax: 10.0,
-	}
-	output, err := usecase.Execute(input)
-	if err != nil{
+	uc := usecases.NewCalculateFinalPrice(orderRepository)
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println(output)
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel) // async process (listening queue), put on second thread
+	RabbitmqWorker(msgRabbitmqChannel, uc)      // in the first thread save the msgs and execute usecases and the second stay listen the queue
+}
+
+func RabbitmqWorker(msgChan chan amqp.Delivery, uc *usecases.CalculateFinalPrice) {
+	fmt.Print("Starting Rabbitmq")
+	for msg := range msgChan {
+		var input usecases.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		out, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println("message saved on database", out)
+	}
 }
